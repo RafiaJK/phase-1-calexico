5a357332420b1669e128e5a1e0b7de35a24e0f58
Rafia Khandaker <rafiakhandaker@gmail.com>
2022-08-15 14:16:29 -0400

    commit tree 61ba1d76b441ff3d8d1938a01b34702933664fae

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your code here...
+fetch ("http://localhost:3000/menu")
+.then(res => res.json())
+.then (challengeOne)
+// .then (challengeTwo) CAN'T CHAIN - FIGURE OUT WHY
+
+function challengeOne(menuItems){
+    for (let i=0; i<menuItems.length; i++){
+        const item = document.createElement("span"); //span elements 
+        const itemName = item.innerHTML = menuItems[i].name; //assign names to the items
+        //console.log(itemName)
+        const menuDiv = document.getElementById('menu-items') //grab menu items
+        //console.log(menuDiv)
+        menuDiv.append(item) //add items to the menu div HTML
+    }
+
+    //Challenge #2 - adding all the image 
+    const imageURL = menuItems[0].image
+    const itemName = menuItems[0].name
+    const itemDescription = menuItems[0].description
+    const itemPrice = menuItems[0].price
+    
+    const dishName = document.getElementById('dish-name')
+    dishName.append(itemName)
+    const dishDescription = document.getElementById('dish-description')
+    dishDescription.append(itemDescription)
+    const dishPrice = document.getElementById('dish-price')
+    dishPrice.append(`$${itemPrice}`)
+    const dishURL = document.getElementById('dish-image')
+    dishURL.src = imageURL
+    
+}
+
+
+// fetch ("http://localhost:3000/menu", {
+//     method: "POST",
+//     headers: {
+//         "Content-Type":"application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//         name: postText
+
+//     })
+// })
